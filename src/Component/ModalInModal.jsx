3b94f8850f34a2df16948311c7e94610ea9eded9
Suraj <Suraj@tekvision.in>
2023-11-22 17:29:41 +0530

    css added second modal

--- src/Component/ModalInModal.jsx
+++ src/Component/ModalInModal.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 export const ModalInModal = ({onHideSecondModal}) => {
 
   const [checked, setChecked] = useState(false);
+
+  const modalInModalFocus = useRef(null)
+
+  useEffect(() => {
+    const secondModalElement = modalInModalFocus.current.querySelectorAll('input, button');
+    secondModalElement[0].focus();
+    console.log(secondModalElement)
+  }, [onHideSecondModal]);
 
   const onSubmit = () => {
     if(checked === true){
@@ -10,12 +18,15 @@ export const ModalInModal = ({onHideSecondModal}) => {
     }};
 
   return (
-    <div role='dialog' aria-modal="true">
+    <>
+    <div className="modal-wrapper" />
+    <div ref = {modalInModalFocus} className="modal-container" role='dialog' aria-modal="true">
       <input aria-labelledby='verify' onChange={() => setChecked(!checked)} type='checkBox' aria-checked={checked} aria-required="true" required/>
         <p id='verify'>
           Please verify that above information is corect by checking the check box.
         </p>
       <button onClick={onSubmit}>Submit</button>
     </div>
+    </>
   )
 };
